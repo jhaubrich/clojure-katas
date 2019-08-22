08655566182808b1a613764172d91dd14b231845
Jesse Haubrich <state_of_the_geos@faa.none>
2019-08-22 07:34:54 -0500

    deftest cards working

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ These katas are sword exercises that will make you one with your editor and envi
 
 Practice daily to commit your clojure workflow to muscle memory. 
 
+TODO
+----
+- [ ] move user functions out of core
+- [ ] move functional redefinition exercises to functional.cljs
+- [ ] add easier exercises, like palandrome, simple filter, simple map problems
+- [ ] setup nrepl
+- [ ] deftest cards are a great start, but I also want people to learn the clojure test suite and build the muscle memory of using their own editor to test :thinking:.
--- src/clojure_katas/core.cljs
+++ src/clojure_katas/core.cljs
@@ -1,15 +1,28 @@
 (ns clojure-katas.core
   (:require
    #_[om.core :as om :include-macros true]
-   [sablono.core :as sab :include-macros true])
+   [sablono.core :as sab :include-macros true]
+   [cljs.test :as t :include-macros true :refer-macros [testing is]])
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest]]))
 
 (enable-console-print!)
 
-(defcard first-card
-  (sab/html [:div
-             [:h1 "This is your first devcard!"]]))
+(defn my-first [col]
+  [])
+
+(deftest test-first
+  "### Implement `(first col)`, a function that returns the first item from `col`"
+  (testing
+    (is (= (my-first [1 2 3 4]) 1))
+    (is (= (my-first []) nil))))
+
+(defcard last
+  "Implement `(last col)`, a function that returns the last item from `col`")
+
+(defcard filter
+  "Implement `(filter fn col)`, a function that takes a function and a collection. `filter` tests each item in `col` with `fn` and returns values of `col` for which `fn` returns true.")
+
 
 (defn main []
   ;; conditionally start the app based on whether the #main-app-area
